f4910aa0822f594c71181cc4b9c5bb640d5e5824
SebasLam3 <90872517+SebasLam3@users.noreply.github.com>
2021-11-16 14:12:49 -0500

    Facturas

--- firebase.js
+++ firebase.js
@@ -30,36 +30,53 @@ function getValueCheckBox(name) {
   });
   return valor
 }
-const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  function generateString(length) {
-    let result = ' ';
-    const charactersLength = characters.length;
-    for ( let i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+function generateString(length) {
+  let result = ' ';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
-
+function required() {
+  var empN = document.getElementById('fname').value
+  var empC =  document.getElementById('email').value
+  var empD = document.getElementById('adr').value
+  var empCi = document.getElementById('ciudad').value
+  var empB = document.getElementById('barrio').value
+  var empT = document.getElementById('telefono').value
+  var empP = document.getElementById('postal').value
+  var empE = document.getElementById('tipoEnvio').value
+  
+  if (empN == "" || empC == "" || empD == "" || empCi == "" ||  empB == ""  || empT == "" || empP == "" || empE == "") {
+    alert("Por favor llene el formulario completo");
+    return false
+  } else {
+    return true
+  }
+}
 document.getElementById('subirDatos').onclick = async function ingresarDatos(idF, productos, nombreUsuario, correoUsuario, direccion, ciudad, barrio, telefono, codPostal, metodoPago, metodo, tipoEnvio, estado) {
 
-  metodo = getValueCheckBox('fooby[1][]')
-  //idF = Math.floor(Math.random()*100);
-  idF = generateString(5)
-  productos = localStorage.getItem('bolsa')
-  nombreUsuario = document.getElementById('fname').value
-  correoUsuario = document.getElementById('email').value
-  direccion = document.getElementById('adr').value
-  ciudad = document.getElementById('ciudad').value
-  barrio = document.getElementById('barrio').value
-  telefono = document.getElementById('telefono').value
-  codPostal = document.getElementById('postal').value
-  metodoPago = metodo[0]
-  tipoEnvio = document.getElementById('tipoEnvio').value
-  estado = "Pendiente"
+  if (required()) {
+    metodo = getValueCheckBox('fooby[1][]')
+    idF = generateString(5)
+    productos = localStorage.getItem('bolsa')
+    nombreUsuario = document.getElementById('fname').value
+    correoUsuario = document.getElementById('email').value
+    direccion = document.getElementById('adr').value
+    ciudad = document.getElementById('ciudad').value
+    barrio = document.getElementById('barrio').value
+    telefono = document.getElementById('telefono').value
+    codPostal = document.getElementById('postal').value
+    metodoPago = metodo[0]
+    tipoEnvio = document.getElementById('tipoEnvio').value
+    estado = "Pendiente"
 
-  const database = getDatabase(app);
-  
-  //Insertar
+
+    const database = getDatabase(app);
+
+    //Insertar
     await set(ref(database, 'Facturas/' + idF), {
       IdFactura: idF,
       Productos: productos,
@@ -74,32 +91,29 @@ document.getElementById('subirDatos').onclick = async function ingresarDatos(idF
       Envio: tipoEnvio,
       Estado: estado
     })
-      .then(() => {
-        // Datos guardados con exito
-        console.log('Datos guardados con exito');
-      })
-      .catch((error) => {
-        // Error al guardar datos
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(error.code + error.message)
-      });
 
-  //Seleccionar data
+    // Datos guardados con exito
+    console.log('Datos guardados con exito');
+    window.location = "factura.html"
+
+    //Seleccionar data
     const dbRef = ref(database);
     get(child(dbRef, 'Facturas/' + idF)).then((snapshot) => {
       if (snapshot.exists()) {
         console.log(snapshot.val());
-        window.location = "factura.html"
       } else {
         console.log("No hay datos asi ")
       }
     })
       .catch((error) => {
-        const errorCode = error.code;
+        error = window.alert("Llene el formulario completo")
+        /*const errorCode = error.code;
         const errorMessage = error.message;
-        console.log(error.code + error.message)
-      }) 
+        console.log(error.code + error.message)*/
+      })
+  }else{
+    window.location = "facturacion.html"
+  }
   //Actualizar
   /*function actualizar() {
     update(ref(database, 'Facturas/' + idF), {
@@ -119,7 +133,5 @@ document.getElementById('subirDatos').onclick = async function ingresarDatos(idF
   }*/
   //insertar()
   //seleccionar()
-  //window.location = "factura.html"
+  formulario.innerHTML = ' '
 };
-
-
